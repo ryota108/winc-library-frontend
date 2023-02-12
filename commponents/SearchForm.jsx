300dd2ce9a231cca8445142ef09b8a835744b4a8
Yuma769 <yuuma6280@akane.waseda.jp>
2023-02-12 12:23:27 +0900

    検索フォーム追記

--- commponents/SearchForm.jsx
+++ commponents/SearchForm.jsx
@@ -1,16 +1,32 @@
 import { Avatar } from '@mui/material'
-import React from 'react'
+import React, { useRef } from 'react'
 
 
 const SearchForm = () => {
+  const ref = useRef();
 
+  const submitHundler = (e) => {
+    e.preventDefault();
+    console.log(ref.current.value);
+    const searchURL = "";
 
+    //fetch data
+    fetch(searchURL)
+      .then((res) => {
+        return res.json();
+      })
+      .then((bookData) => {
+        setFetchData(bookData);
+      });
+  };
 
   return (
     <div className='search_wrapper'>
      <h1 className='logo'>WINC Library</h1>
       <Avatar className='user_avatar'/>
-      <input className='search_input' placeholder='Search books...'/>
+      <form onSubmit={submitHundler}>
+        <input type="text" className='search_input' placeholder='Search books...' ref={ref}/>
+      </form>
       </div>
   )
 }

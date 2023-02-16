da4bf491fffb356da47594440a9e7f04619d56b6
Yuma769 <yuuma6280@akane.waseda.jp>
2023-02-16 22:27:43 +0900

    トップデザイン修正

--- commponents/SearchForm.jsx
+++ commponents/SearchForm.jsx
@@ -1,6 +1,7 @@
-import { Avatar } from '@mui/material'
+import { Avatar, IconButton, TextField } from '@mui/material'
 import React, { useRef, useState } from 'react'
 import { useRouter } from 'next/router';
+import SearchIcon from '@mui/icons-material/Search';
 
 const SearchForm = () => {
   const ref = useRef();
@@ -17,7 +18,16 @@ const SearchForm = () => {
      <h1 className='logo'>WINC Library</h1>
       <Avatar className='user_avatar'/>
       <form onSubmit={submitHundler}>
-        <input type="text" className='search_input' placeholder='Search books...' ref={ref}/>
+        <TextField 
+          InputProps={{
+            endAdornment: (
+              <SearchIcon sx={{fontSize:"20px", color:"#9c9c9c"}}/>  
+            ),
+          }}
+          sx={{"& fieldset": { border:"none" }}}
+          className='search_input' 
+          placeholder='Search books...' 
+          inputRef={ref}/>
       </form>
     </div>
   )

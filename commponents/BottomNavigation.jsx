cd80fb0dc7604d9fda66f41678738a1be6cb34a3
Yuma769 <yuuma6280@akane.waseda.jp>
2023-02-18 03:24:18 +0900

    ボトムナビゲーションの色を変えました。

--- commponents/BottomNavigation.jsx
+++ commponents/BottomNavigation.jsx
@@ -6,24 +6,36 @@ import HomeIcon from '@mui/icons-material/Home';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
 import Link from "next/link"
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@emotion/react';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#000000"
+    }
+  }
+});
 
 export default function SimpleBottomNavigation() {
   const [value, setValue] = React.useState(0);
 
   return (
-    <Box sx={{ width: "100%",position:"fixed",bottom:0 }}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-        className="bottomNavigation"
-      >
-        <BottomNavigationAction LinkComponent={Link} href="/"   icon={<HomeIcon  />} />
-        <BottomNavigationAction LinkComponent={Link} href="/favorite" icon={<FavoriteBorderIcon  />} />
-        <BottomNavigationAction LinkComponent={Link} href="/mypage" icon={<MenuBookIcon />} />
-      </BottomNavigation>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ width: "100%",position:"fixed",bottom:0 }}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+          className="bottomNavigation"
+        >
+          <BottomNavigationAction LinkComponent={Link} href="/"   icon={<HomeIcon/>} />
+          <BottomNavigationAction LinkComponent={Link} href="/favorite" icon={<FavoriteBorderIcon  />} />
+          <BottomNavigationAction LinkComponent={Link} href="/mypage" icon={<MenuBookIcon />} />
+        </BottomNavigation>
+      </Box>
+    </ThemeProvider>
   );
 }

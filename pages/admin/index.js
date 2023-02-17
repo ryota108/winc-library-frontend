60660d86897be22e35040766ae1ff818db3d63bc
Ryota <aya.ryo.718@outlook.jp>
2023-02-17 23:12:01 +0900

    fix:deploy-error解消

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -2,13 +2,11 @@ import React from 'react'
 import AdminBook from '../../commponents/AdminBook'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import SimpleBottomNavigation from '../../commponents/BottomNavigation';
-import { useRouter } from 'next/router';
 
 const index = () => {
-  const router = useRouter()
   return (
     <>
-     <ArrowBackIcon size='30px' className='arrowBack' onClick={() => router.back()}/>
+     <ArrowBackIcon size='30px' className='arrowBack'/>
     <h1 className='adminPage_title'>書籍管理</h1>
     <div style={{display:"flex",flexWrap:"wrap",marginLeft:"3%",marginTop:"15%"}}>
     <AdminBook/>
--- pages/favorite/index.js
+++ pages/favorite/index.js
@@ -3,13 +3,11 @@ import SimpleBottomNavigation from "../../commponents/BottomNavigation";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Book from "../../commponents/Book";
 import { Box } from "@mui/system";
-import { useRouter } from 'next/router'
 
 const index = () => {
-    const router = useRouter()
   return (
     <div>
-      <ArrowBackIcon size="30px" className="arrowBack" onClick={() => router.back()}/>
+      <ArrowBackIcon size="30px" className="arrowBack"/>
       <h1 className='adminPage_title'>お気に入りした本</h1>
       <Box sx={{marginTop:"10%",display:"flex",marginLeft:"10%"}}>
       <Book imgUrl="https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/7473/9784297127473_1_3.jpg?_ex=120x120"

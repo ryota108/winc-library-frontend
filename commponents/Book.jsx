0dcdad2406e7b93def8c91ea6a0b76dea1cb44eb
Ryota <aya.ryo.718@outlook.jp>
2023-02-18 02:40:08 +0900

    feature:本詳細画面の追加

--- commponents/Book.jsx
+++ commponents/Book.jsx
@@ -2,20 +2,27 @@ import { Button, IconButton } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import "../styles/book.module.css";
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import Link from "next/link";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
-const Book = ({ imgUrl, title, author }) => {
-  const enoughTitle = title?.length > 10
-  const enoughAuthor = author?.length < 10
-  const shortenTitle = title?.substr(0,27);
-  const shortenAuthor = author?.substr(0,11);
+const Book = ({ imgUrl, title, author,id }) => {
+  const enoughTitle = title?.length > 10;
+  const enoughAuthor = author?.length < 10;
+  const shortenTitle = title?.substr(0, 27);
+  const shortenAuthor = author?.substr(0, 11);
   return (
     <Box className="book_wrapper">
-      <img src={imgUrl} className="book" />
-      <p className={`book-title ${enoughTitle ? "long":""}`}>{`${shortenTitle}${title.length>28 ?"...":""}`}</p>
-      <p className="book-author">{`${shortenAuthor}${enoughAuthor?"":"..."}`}</p>
-        <FavoriteBorderIcon sx={{fontSize:"15px"}} className="book_like"/>
-    </Box>
+        <Link href={`/detail/${id}`}>
+        <img src={imgUrl} className="book" />
+        <p
+          className={`book-title ${enoughTitle ? "long" : ""}`}
+        >{`${shortenTitle}${title.length > 28 ? "..." : ""}`}</p>
+        <p className="book-author">{`${shortenAuthor}${
+          enoughAuthor ? "" : "..."
+        }`}</p>
+        <FavoriteBorderIcon sx={{ fontSize: "15px" }} className="book_like" />
+    </Link>
+      </Box>
   );
 };
 
--- pages/detail/[id].jsx
+++ pages/detail/[id].jsx
@@ -5,30 +5,35 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import PersonIcon from '@mui/icons-material/Person';
+import Link from "next/link";
 
 
-const Detail = () => {
+const Detail = ({data}) => {
   const [isFavorite,setIsFavorite] = useState(false)
   const [isRental,setIsRental] = useState(false)
   const favoriteHandler = () => {
     setIsFavorite((prev)=>{setIsFavorite(!prev)})
   }
   const RentalHandler = () => {
-    setIsRental(true)
-  }
-
-  return (
-    <Box className="detail-wrapper">
+      setIsRental(true)
+    }
+    const detail = data?.Items[0].Item
+    console.log(data)
+    
+    return (
+        <Box className="detail-wrapper">
       {isFavorite ? <FavoriteIcon className="book_detail_favorite" onClick={favoriteHandler}/> : <FavoriteBorderIcon className="book_detail_favorite not_favorite" onClick={favoriteHandler}/> }
       <IconButton aria-label="back" sx={{mt:"20px", ml:"20px"}}>
-        <ArrowBackIcon fontSize="small" sx={{color:"#000000"}}/>
+        <Link href="/">
+        <ArrowBackIcon fontSize="small" sx={{color:"#000000"}} />
+        </Link>
       </IconButton>
-      <img src="https://m.media-amazon.com/images/I/616AVqqSP1L._AC_SS260_.jpg" className="detail-image"/>
-      <p className="book-detail_title">Python入門</p> 
+      <img src={detail?.largeImageUrl} className="detail-image"/>
+      <p className="book-detail_title">{detail?.title}</p> 
       <Box sx={{ display: "flex" }}>
         <div className="detail_info_wrapper">
           <p className="detail_info_title">値段</p>
-          <p className="detail_info_data">2240円</p>
+          <p className="detail_info_data">{detail?.itemPrice}円</p>
         </div>
         <div className="detail_info_wrapper">
           <p className="detail_info_title">リクエスト数</p>
@@ -36,17 +41,17 @@ const Detail = () => {
         </div>
         <div className="detail_info_wrapper">
           <p className="detail_info_title">評価</p>
-          <p className="detail_info_data">4/5</p>
+          <p className="detail_info_data">{detail?.reviewAverage}/5</p>
         </div>
       </Box>
       <Box>
-        <p className="detail_description">本企画は、TypeScriptをハンズオン形式で学ぶ書籍です。</p> 
+        <p className="detail_description"> {detail?.itemCaption}</p> 
       </Box>
       <Box>
         <ul>
-          <li className="detail_book_data">著者 : 山田太郎</li>
-          <li className="detail_book_data">出版社 : 秀和システム</li>
-          <li className="detail_book_data">ISBN-10 : 4798065331</li>
+          <li className="detail_book_data">著者 : {detail?.author}</li>
+          <li className="detail_book_data">出版社 :  {detail?.publisherName}</li>
+          <li className="detail_book_data">ISBN-10 : {detail?.isbn}</li>
         </ul>
       </Box>
       <Button
@@ -55,7 +60,7 @@ const Detail = () => {
           // type="submit"
           color="primary"
           variant="outlined"
-        >
+          >
         <p>
         リクエストする
         </p>
@@ -63,28 +68,40 @@ const Detail = () => {
       </Button>
       {/* <LoadingButton
           className="detail-button"
-    
+          
           type="submit"
           color="primary"
           variant="outlined"
-        >
-        <p>
-        借りる
-        </p>
-      </LoadingButton>
-      <LoadingButton
+          >
+          <p>
+          借りる
+          </p>
+          </LoadingButton>
+          <LoadingButton
           className="detail-button"
-    
+          
           type="submit"
           color="primary"
           variant="outlined"
-        >
-        <p>
-        予約する
-        </p>
-      </LoadingButton> */}
+          >
+          <p>
+          予約する
+          </p>
+        </LoadingButton> */}
     </Box>
   );
 };
+
+export async function getServerSideProps(context) {
+    const baseURL = `https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404?applicationId=${process.env.NEXT_PUBLIC_SECRETKEY}`
+    const id = context.params.id;
+    const res = await fetch(baseURL+"&isbnjan=" + id);
+    const data = await res.json();
+    return {
+        props: {
+            data,
+        },
+    };
+  }
 
 export default Detail;
